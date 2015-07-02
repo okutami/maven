30e1cb57be05e4bdae4761eebab562ef4c7224dc
okutami <gitokutami44@gmail.com>
2015-07-02 10:19:28 +0900

    コマンドラインオプションで grep オプションが指定された場合の処理を追加する

--- src/main/java/com/github/finder/Finder.java
+++ src/main/java/com/github/finder/Finder.java
@@ -30,8 +30,25 @@ public class Finder {
         if(args.getSize() != null){
             flag &= checkTargetSize(file, args.getSize());
         }
+        if(args.getGrep() != null){
+            flag &= checkGrep(file, args.getGrep());
+        }
 
         return flag;
+    }
+
+    private boolean checkGrep(File file, String pattern){
+        if(file.isFile()){
+            try(BufferedReader in = new BufferedReader(new FileReader(file))){
+                String line;
+                while((line = in.readLine()) != null){
+                    if(line.indexOf(pattern) >= 0){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
     private boolean checkTargetSize(File file, String sizeString){
         if(file.isFile()){
